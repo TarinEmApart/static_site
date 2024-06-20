3b74f99d65372db1f81a8ae42ded420a29123f96
Tarin <matt.izzo813@gmail.com>
2024-06-19 20:23:42 -0600

    added textnode converter function

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -16,3 +18,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def text_node_to_html(text_node):
+        values = ["text", "bold", "italic", "code", "link", "image"]
+        if text_node.text_type not in values:
+            raise Exception("Invalid text type.")
+        elif text_node.text_type == "text":
+            html_node = LeafNode(None, text_node.text, None)
+        elif text_node.text_type == "bold":
+            html_node = LeafNode("b", text_node.text, None)
+        elif text_node.text_type == "italic":
+            html_node = LeafNode("i", text_node.text, None)
+        elif text_node.text_type == "code":
+            html_node = LeafNode("code", text_node.text, None)
+        elif text_node.text_type == "link":
+            html_node = LeafNode("a", text_node.text, {"href" : text_node.url})
+        elif text_node.text_type == "image":
+            html_node = LeafNode("img", "", {"src" : text_node.url, "alt" :text_node.text})
+        return html_node
